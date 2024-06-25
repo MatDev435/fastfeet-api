74ea1b3ad91daf469a8f026a2e7fb21f2aa8f571
MatDev435 <matheuspsalmazo2@gmail.com>
2024-06-25 14:48:29 -0300

    Initial commit

---
+++ src/domain/users/enterprise/entities/Admin.ts
@@ -0,0 +1,40 @@
+import { Entity } from 'src/core/entities/entity'
+import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
+
+export interface AdminProps {
+  name: string
+  cpf: string
+  password: string
+}
+
+export class Admin extends Entity<AdminProps> {
+  get name() {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+  }
+
+  get cpf() {
+    return this.props.cpf
+  }
+
+  set cpf(cpf: string) {
+    this.props.cpf = cpf
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  set password(password: string) {
+    this.props.password = password
+  }
+
+  static create(props: AdminProps, id?: UniqueEntityID) {
+    const admin = new Admin(props, id)
+
+    return admin
+  }
+}
---
+++ src/domain/users/enterprise/entities/courier.ts
@@ -0,0 +1,40 @@
+import { Entity } from 'src/core/entities/entity'
+import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
+
+export interface CourierProps {
+  name: string
+  cpf: string
+  password: string
+}
+
+export class Courier extends Entity<CourierProps> {
+  get name() {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+  }
+
+  get cpf() {
+    return this.props.cpf
+  }
+
+  set cpf(cpf: string) {
+    this.props.cpf = cpf
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  set password(password: string) {
+    this.props.password = password
+  }
+
+  static create(props: CourierProps, id?: UniqueEntityID) {
+    const courier = new Courier(props, id)
+
+    return courier
+  }
+}
